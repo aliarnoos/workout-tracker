8e20b4a6b617224ae28d7bef336bbc3c0f2410ee
aliarnoos <alimarnoos@gmail.com>
2022-12-29 15:15:48 +0300

    fix mobile view

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,42 +11,40 @@ function NavBar() {
 
   return (
     <>
-      <header>
-        <div className="font-bold text-4xl p-8 pl-24 text-gray-900 bg-white tracking-tighter flex justify-between align-center">
-          <Link to={"/workout-tracker"}>
-            <h1>Workout Tarcker</h1>
-          </Link>
-          <nav>
-            {user && (
-              <div className="text-lg p-4 pl-24 text-gray-900 bg-white tracking-tighter flex gap-6">
-                <span>{user.email}</span>
-                <button
-                  onClick={handleClick}
-                  className="border-2 rounded	text-green-500 border-green-500 p-1"
-                >
-                  Log out
-                </button>
-              </div>
-            )}
-            {!user && (
-              <div className="text-lg p-4 pl-24 text-gray-900 bg-white tracking-tighter flex gap-6">
-                <Link
-                  to="/login"
-                  className="border-2 rounded	text-green-500 border-green-500 p-1"
-                >
-                  Log in
-                </Link>
-                <Link
-                  to="/signup"
-                  className="border-2 rounded	text-white bg-green-500 border-green-500 p-1"
-                >
-                  Sign up
-                </Link>
-              </div>
-            )}
-          </nav>
-        </div>
-      </header>
+      <div className="font-bold text-4xl p-8  text-gray-900 bg-white tracking-tighter flex justify-between align-center max-md:flex-col max-md:justify-center max-md:pl-8">
+        <Link to={"/workout-tracker"}>
+          <h1 className="text-center">Workout Tarcker</h1>
+        </Link>
+        <nav className="flex justify-center align-center ">
+          {user && (
+            <div className="text-lg p-4 text-gray-900 bg-white tracking-tighter flex gap-6">
+              <span>{user.email}</span>
+              <button
+                onClick={handleClick}
+                className="border-2 rounded	text-green-500 border-green-500 p-1"
+              >
+                Log out
+              </button>
+            </div>
+          )}
+          {!user && (
+            <div className="text-lg p-4 text-gray-900 bg-white tracking-tighter flex gap-6 justify-center align-center md:pl-4">
+              <Link
+                to="/login"
+                className="border-2 rounded	text-green-500 border-green-500 p-1"
+              >
+                Log in
+              </Link>
+              <Link
+                to="/signup"
+                className="border-2 rounded	text-white bg-green-500 border-green-500 p-1"
+              >
+                Sign up
+              </Link>
+            </div>
+          )}
+        </nav>
+      </div>
     </>
   );
 }
